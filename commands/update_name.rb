c080ec7a896e577f8b4219ca63de577ddb1e4031
Rhenium <rhenium@rhe.jp>
2014-01-24 17:34:43 +0900

    commands/update_name: fix

--- commands/update_name.rb
+++ commands/update_name.rb
@@ -1,16 +1,22 @@
 require "cgi"
 
-update_name = proc do |json, *match|
-  str = CGI.unescapeHTML(json[:text].sub(match[1], ""))
+update_name = ->(str, json) do
   begin
     twitter.update_profile(name: str)
-    twitter.update(str.gsub(/[@＠]/, ""))
+    twitter.update(str.gsub(/[@＠]/, "(at)"))
   rescue
     twitter.update("@#{json[:user][:screen_name]} 表示名を更新できませんでした(∩´﹏`∩)",
                    in_reply_to_status_id: json[:id])
   end
 end
 
-on_tweet(/^[@＠]#{user.screen_name}\s+update_name\s*(.+)$/, &update_name)
-on_tweet(/([(（][@＠]#{user.screen_name}[)）])/, &update_name)
+on_tweet(/^[@＠]#{user.screen_name}\s+update_name\s*(.+)$/) do |json, *match|
+  str = CGI.unescapeHTML(match[1])
+  update_name.call(str, json)
+end
+
+on_tweet(/([(（][@＠]#{user.screen_name}[)）])/) do |json, *match|
+  str = CGI.unescapeHTML(json[:text].sub(match[1], ""))
+  update_name.call(str, json)
+end
 
