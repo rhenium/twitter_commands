a92a2c091f4b4a338eabd92b9ef48aaad6825e39
Rhenium <rhenium@rhe.jp>
2014-01-28 18:04:44 +0900

    commands/update_name: change log message

--- commands/update_name.rb
+++ commands/update_name.rb
@@ -4,11 +4,11 @@ def update_name(str, json)
   user = twitter.update_profile(name: str)
   twitter.update("@#{json[:user][:screen_name]} #{user.name.gsub(/[@＠]/, "(at)")}になりました",
                  in_reply_to_status_id: json[:id])
-  logger.info "Successfully changed name to \"#{user.name}\" (triggered by #{json[:user][:screen_name]})"
+  logger.info "Successfully updated name to \"#{user.name}\" (triggered by #{json[:user][:screen_name]})"
 rescue
   twitter.update("@#{json[:user][:screen_name]} 表示名を更新できませんでした(∩´﹏`∩)",
                  in_reply_to_status_id: json[:id])
-  logger.info "Failed to change name to \"#{str}\" (triggered by #{json[:user][:screen_name]})"
+  logger.info "Failed to update name to \"#{str}\" (triggered by #{json[:user][:screen_name]})"
 end
 
 on_tweet(/^[@＠]#{screen_name}\s+update_name\s*(.+)$/) do |json, match|
