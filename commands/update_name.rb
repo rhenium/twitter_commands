b88db9121032012ebaa746b5c5a164d39a7d5dfb
Rhenium <rhenium@rhe.jp>
2014-01-28 17:40:02 +0900

    commands/update_name: brush up

--- commands/update_name.rb
+++ commands/update_name.rb
@@ -1,23 +1,23 @@
 require "cgi"
 
-update_name = ->(str, json) do
-  begin
-    twitter.update_profile(name: str)
-    twitter.update("@#{json[:user][:screen_name]} #{str.gsub(/[@＠]/, "(at)")} になりました",
-                   in_reply_to_status_id: json[:id])
-  rescue
-    twitter.update("@#{json[:user][:screen_name]} 表示名を更新できませんでした(∩´﹏`∩)",
-                   in_reply_to_status_id: json[:id])
-  end
+def update_name(str, json)
+  user = twitter.update_profile(name: str)
+  twitter.update("@#{json[:user][:screen_name]} #{user.name.gsub(/[@＠]/, "(at)")}になりました",
+                 in_reply_to_status_id: json[:id])
+  logger.info "Successfully changed name to \"#{user.name}\" (triggered by #{json[:user][:screen_name]})"
+rescue
+  twitter.update("@#{json[:user][:screen_name]} 表示名を更新できませんでした(∩´﹏`∩)",
+                 in_reply_to_status_id: json[:id])
+  logger.info "Failed to change name to \"#{str}\" (triggered by #{json[:user][:screen_name]})"
 end
 
-on_tweet(/^[@＠]#{user.screen_name}\s+update_name\s*(.+)$/) do |json, *match|
+on_tweet(/^[@＠]#{screen_name}\s+update_name\s*(.+)$/) do |json, match|
   str = CGI.unescapeHTML(match[1])
-  update_name.call(str, json)
+  update_name(str, json)
 end
 
-on_tweet(/([(（][@＠]#{user.screen_name}[)）])/) do |json, *match|
+on_tweet(/([(（][@＠]#{screen_name}[)）])/) do |json, match|
   str = CGI.unescapeHTML(json[:text].sub(match[1], ""))
-  update_name.call(str, json)
+  update_name(str, json)
 end
 
