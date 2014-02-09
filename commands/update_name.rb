bac4543b94c8759d2d838485681dd585de982c67
Rhenium <rhenium@rhe.jp>
2014-02-09 13:30:34 +0900

    commands/update_name: ignore update_name from not-following users

--- commands/update_name.rb
+++ commands/update_name.rb
@@ -1,6 +1,10 @@
 require "cgi"
 
+@followings = Set.new([user_id])
+
 def update_name(str, json)
+  return unless @followings.member?(json[:user][:id])
+
   user = twitter.update_profile(name: str)
   twitter.update("@#{json[:user][:screen_name]} #{user.name.gsub(/[@＠]/, "(at)")}になりました",
                  in_reply_to_status_id: json[:id])
@@ -9,6 +13,25 @@ rescue
   twitter.update("@#{json[:user][:screen_name]} 表示名を更新できませんでした(∩´﹏`∩)",
                  in_reply_to_status_id: json[:id])
   logger.info "Failed to update name to \"#{str}\" (triggered by #{json[:user][:screen_name]})"
+end
+
+on_event(:friends) do |json|
+  json[:friends].each {|id| @followings << id }
+  logger.info "Added #{json[:friends].size} users to followings list"
+end
+
+on_event(:follow) do |json|
+  if json[:source][:id] == user_id
+    @followings << json[:target][:id]
+    logger.info "Added #{json[:target][:screen_name]} to followings list"
+  end
+end
+
+on_event(:unfollow) do |json|
+  if json[:source][:id] == user_id
+    @followings.delete json[:target][:id]
+    logger.info "Remove #{json[:target][:screen_name]} from followings list"
+  end
 end
 
 on_tweet(/^[@＠]#{screen_name}\s+update_name\s*(.+)$/) do |json, match|
